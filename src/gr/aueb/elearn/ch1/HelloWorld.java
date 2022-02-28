5d40b3508f33e78bc9985c42b98153922fc79169
MariaB <mar.binia1@gmail.com>
2022-02-28 16:16:42 +0200

    Hello World v 0.1

---
+++ src/gr/aueb/elearn/ch1/HelloWorld.java
@@ -0,0 +1,16 @@
+package gr.aueb.elearn.ch1;
+
+/*
+ * Prints on the screen the phrase "Hello World"
+ *
+ * @author M. Binia
+ * @version 0.1
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+
+}
