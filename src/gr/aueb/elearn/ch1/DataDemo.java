848fa5e23511bdc6cedb96ffedf0cdbf6fb1394b
MariaB <mar.binia1@gmail.com>
2022-02-28 16:43:22 +0200

    Data Demo v.0.1

---
+++ src/gr/aueb/elearn/ch1/DataDemo.java
@@ -0,0 +1,34 @@
+/*
+ * File DataDemo 0.1 23/02/2022
+ *
+ * Copyright (c) M. Binia
+ *
+ * All rights reserved.
+ */
+
+
+package gr.aueb.elearn.ch1;
+
+/*
+ * Calculates and prints the sum
+ * of two numbers 12 and 5
+ *
+ * @author Maria Binia
+ */
+
+public class DataDemo {
+
+    public static void main(String[] args) {
+        // Initiation and declaration of variables
+        int num1 = 12;
+        int num2 = 5;
+        int sum;
+
+        // Commands
+        sum = num1 + num2;
+
+        // Print results
+        System.out.println("The sum of 12 and 5 is = " + sum);
+    }
+
+}
